5ba43a846e96a548673cb0a4ad9859bcd6f15261
Rohitbista <114638542+Rohitbista@users.noreply.github.com>
2025-11-13 14:25:14 +0545

    Added features to add and remove notes

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Notes(BaseModel):
+    id: int
+    topic: str
+    content: str
+
+db = {}
 
 @app.get("/")
 def root():
     return {"message":"this is a new project"}
+
+@app.post("/add_note")
+def add_a_note(note: Notes):
+    db["id"] = note.id
+    db["topic"] = note.topic
+    db["content"] = note.content
+    return {"message":"successfully entered the note in the db"}
+
+@app.delete("/delete_by_ID/{note_id}")
+def delete_a_note(note_id: int):
+    if note_id in db:
+        del db[note_id]
+        return "Note has been deleted"
+    raise HTTPException(status_code=404, detail="Note ID not does not exist")
+
+
+
+
+
